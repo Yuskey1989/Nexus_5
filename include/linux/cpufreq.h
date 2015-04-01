8226feeefcda39bd9e6da1ca7250a47adc04608a
Yusuke Fukutsuka <donsuke.f@gmail.com>
2015-04-01 16:40:32 +0900

    cpufreq: interest: Add Interest cpu governor
    This cpu governor is based on interactive governor.
    It is merged some features and upstream patches.
    
    * Sync freq
        If the max load among other CPUs is higher than up_threshold_any_cpu_load and
        if the highest frequency among the other CPUs is higher than up_threshold_any_cpu_freq,
        do not let the frequency to drop below sync_freq.
    * Self input boost capability from input drivers
        No need for PowerHAL assist.
        Input boost freq is the same as hispeed freq.
    * Go lowspeed load
        Go to lowest speed when CPU load at or below this value.
    * Remove tracing
    * "soft" io_is_busy (value can be set above 1)
    * Improve boost logic
    * Separate input boost freq and duration from hispeed freq
    * powersave_bias
        The governor emulates ondemand's powersave_bias with using CPUFREQ_RELATION_C.
        New frequency is calculated by new freq -= new freq * powersave_bias / 1000, and
        it is decided from closest freq table.
        The default range is from -1000 to 1000.
    * plus_ondemand
        While plus_ondemand is enabled, the governor emulates ondemand governor's feature.
        If cpu load exceeds hispeed_load when current frequency is above hispeed frequency,
        next frequency is set to max frequency.
        When this value is greater than 1, next frequency is not set to max frequency
        until high load and high speed continue this value times.
    * plus_smart
        While plus_smart is enabled, the governor emulates smart* governor's feature.
        When below the hispeed_freq we always ramp up to the hispeed_freq.
        When above the hispeed_freq we always ramp down to the hispeed_freq.
        When this value is greater than 1, enable smart mode only for
        ramping down from above hispeed_freq.
    * ramp_up_step
        After next frequency is calculated by target load,
        next frequency goes up to current frequency + this value (kHz).
    * ramp_down_step
        After next frequency is calculated by target load,
        next frequency goes down to current frequency - this value (kHz).
    
        We are able to mix load heuristic and frequency step based works
        for each ramping up and down.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -411,6 +411,9 @@ extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
 extern struct cpufreq_governor cpufreq_gov_intellimm;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellimm)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTEREST)
+extern struct cpufreq_governor cpufreq_gov_interest;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interest)
 #endif
 
 
