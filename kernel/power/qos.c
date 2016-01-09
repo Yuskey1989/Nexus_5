2f821c7c3295c2caacefdb4fc9c4bf1d00fc2e61
Yusuke Fukutsuka <donsuke.f@gmail.com>
2016-01-10 01:38:12 +0900

    Revert "PM / QoS: Fix workqueue deadlock when using pm_qos_update_request_timeout()"
    This reverts commit d325959ad4974094a2140faa5ca785924ef4b012.

--- kernel/power/qos.c
+++ kernel/power/qos.c
@@ -229,15 +229,6 @@ int pm_qos_request_active(struct pm_qos_request *req)
 }
 EXPORT_SYMBOL_GPL(pm_qos_request_active);
 
-static void __pm_qos_update_request(struct pm_qos_request *req,
-			   s32 new_value)
-{
-	if (new_value != req->node.prio)
-		pm_qos_update_target(
-			pm_qos_array[req->pm_qos_class]->constraints,
-			&req->node, PM_QOS_UPDATE_REQ, new_value);
-}
-
 /**
  * pm_qos_work_fn - the timeout handler of pm_qos_update_request_timeout
  * @work: work struct for the delayed work (timeout)
@@ -246,6 +237,7 @@ static void __pm_qos_update_request(struct pm_qos_request *req,
  */
 static void pm_qos_work_fn(struct work_struct *work)
 {
+	s32 new_value = PM_QOS_DEFAULT_VALUE;
 	struct pm_qos_request *req = container_of(to_delayed_work(work),
 						  struct pm_qos_request,
 						  work);
@@ -253,7 +245,10 @@ static void pm_qos_work_fn(struct work_struct *work)
 	if (!req || !pm_qos_request_active(req))
 		return;
 
-	__pm_qos_update_request(req, PM_QOS_DEFAULT_VALUE);
+	if (new_value != req->node.prio)
+		pm_qos_update_target(
+			pm_qos_array[req->pm_qos_class]->constraints,
+			&req->node, PM_QOS_UPDATE_REQ, new_value);
 }
 
 /**
@@ -314,8 +309,6 @@ void pm_qos_update_request(struct pm_qos_request *req,
 		pm_qos_update_target(
 			pm_qos_array[req->pm_qos_class]->constraints,
 			&req->node, PM_QOS_UPDATE_REQ, new_value);
-
-	__pm_qos_update_request(req, new_value);
 }
 EXPORT_SYMBOL_GPL(pm_qos_update_request);
 
